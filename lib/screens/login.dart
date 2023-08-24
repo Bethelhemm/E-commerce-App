bde85d3d43e2e9941bdb20c909e36e15e8547d2c
edl ghana <eduedil30@gmail.com>
2023-08-24 13:57:32 +0300

    added_login_as_member

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,87 +8,284 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 255, 245, 240),
-        leading: IconButton(
-            onPressed: () {},
-            color: Colors.brown,
-            icon: const Icon(Icons.arrow_back)),
-        title: const Text(
-          "Log In",
-          style: TextStyle(color: Colors.brown),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          title: const Text(
+            "goodReads",
+            style: TextStyle(fontSize: 34, color: Colors.brown),
+          ),
+          centerTitle: true,
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 40),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const TextField(
-            decoration: InputDecoration(labelText: 'E-mail'),
-          ),
-          const SizedBox(height: 16.0),
-          const TextField(
-            decoration: InputDecoration(
-              suffixIcon: Icon(Icons.remove_red_eye),
-              labelText: 'Goodreads password',
-            ),
-            obscureText: true,
-          ),
-          const SizedBox(
-            height: 16.0,
-          ),
-          const Text(
-            "Forgot your password?",
-            style: TextStyle(
-                fontSize: 12, color: Color.fromARGB(255, 106, 160, 253)),
-          ),
-          const SizedBox(
-            height: 16.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Column(
             children: [
-              Expanded(
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.all(18.0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        )),
-                    onPressed: () {},
-                    child: const Text("Log In")),
+              const SizedBox(
+                height: 16,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Sign in",
+                    style: TextStyle(
+                        fontFamily: AutofillHints.telephoneNumberLocalSuffix,
+                        fontSize: 27),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                        text: "Forgot password?",
+                        style: const TextStyle(
+                            decoration: TextDecoration.underline),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            const url =
+                                'https://www.heroku.com/'; // Replace with your Terms of Service URL
+                            launchUrl(url as Uri);
+                          }),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 224, 218, 218),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'email',
+                    contentPadding: EdgeInsets.all(10.0),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 224, 218, 218),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'password',
+                    contentPadding: EdgeInsets.all(10.0),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.check_box_outline_blank_sharp,
+                            size: 25,
+                          )),
+                      const Text(
+                        "Show password.",
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            padding: const EdgeInsets.all(18.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            )),
+                        onPressed: () {},
+                        child: const Text("Sign in")),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: 'By signing in, you agree to the Goodreads',
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Terms of Service',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        decoration: TextDecoration.underline,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          const url =
+                              'https://example.com/terms-of-service'; // Replace with your Terms of Service URL
+                          launchUrl(url as Uri);
+                        },
+                    ),
+                    const TextSpan(text: ' and '),
+                    TextSpan(
+                      text: 'Privacy Policy',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        decoration: TextDecoration.underline,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          const url =
+                              'https://example.com/privacy-policy'; // Replace with your Privacy Policy URL
+                          launchUrl(url as Uri);
+                        },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                color: Colors.white,
+                child: (const Row(
+                  children: <Widget>[
+                    // ...
+                    Expanded(
+                      child: Column(
+                        children: <Widget>[
+                          Text("New to ?"),
+                          Divider(color: Colors.black)
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                          padding: const EdgeInsets.all(18.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          )),
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 100, 100, 100)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(
+                          TextSpan(children: [
+                            TextSpan(
+                              text: ' Terms of Service',
+                              style: const TextStyle(
+                                fontSize: 18,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  const url =
+                                      'https://example.com/terms-of-service'; // Replace with your Terms of Service URL
+                                  launchUrl(url as Uri);
+                                },
+                            ),
+                            const TextSpan(
+                              text: ' ',
+                              style: TextStyle(fontSize: 100),
+                            ),
+                            TextSpan(
+                              text: ' Privacy',
+                              style: const TextStyle(
+                                fontSize: 18,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  const url =
+                                      'https://example.com/terms-of-service'; // Replace with your Terms of Service URL
+                                  launchUrl(url as Uri);
+                                },
+                            ),
+                            const TextSpan(
+                              text: ' ',
+                              style: TextStyle(fontSize: 100),
+                            ),
+                            TextSpan(
+                              text: ' Help',
+                              style: const TextStyle(
+                                fontSize: 18,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  const url =
+                                      'https://example.com/terms-of-service'; // Replace with your Terms of Service URL
+                                  launchUrl(url as Uri);
+                                },
+                            ),
+                          ]),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                    const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.copyright,
+                              size: 14,
+                              color: Colors.grey,
+                            ),
+                            Text("2023 Goodreads, Inc."),
+                          ],
+                        ))
+                  ],
+                ),
               ),
             ],
           ),
-          const SizedBox(
-            height: 16.0,
-          ),
-          const Expanded(
-            child: Text.rich(TextSpan(
-              text: "By using Goodreads,you agree to our",
-              children: [
-                TextSpan(
-                  text: ' Terms of Use, Privacy Policy ',
-                  style: TextStyle(
-                    color: Color.fromARGB(
-                        255, 33, 205, 228), // Change the color here
-                  ),
-                ),
-                TextSpan(
-                  text: ' and ',
-                ),
-                TextSpan(
-                  text: ' Ads Policy ',
-                  style: TextStyle(
-                    color: Color.fromARGB(
-                        255, 33, 205, 228), // Change the color here
-                  ),
-                ),
-                
-              ],
-            )),
-          ),
-        ]),
-      ),
-    );
+        ));
   }
 }
