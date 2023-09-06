3dc0494982d144b29a1c2724c3d14481cdb337b5
Bethelhemm <122419774+Bethelhemm@users.noreply.github.com>
2023-09-06 12:11:27 +0300

    Add files via upload

---
+++ lib/Models/genre.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'genre.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Genre _$$_GenreFromJson(Map<String, dynamic> json) => _$_Genre(
+      GenreName: json['GenreName'] as String,
+      GenreImage: json['GenreImage'] as String,
+      GenreId: json['GenreId'] as String,
+    );
+
+Map<String, dynamic> _$$_GenreToJson(_$_Genre instance) => <String, dynamic>{
+      'GenreName': instance.GenreName,
+      'GenreImage': instance.GenreImage,
+      'GenreId': instance.GenreId,
+    };
